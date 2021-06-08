451a70193dbf1dd861ae8d1179cf361b362a456e
Nicolas Posa <nicoposa57@gmail.com>
2021-06-08 19:50:23 -0300

    creando el tab de la app

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import React from "react";
 import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
+
 import CoinsStack from "./src/components/coins/CoinsStack";
+import Colors from "./src/res/colors";
+import { Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: "#fefefe",
+          style: {
+            backgroundColor: Colors.blackPearl,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Image
+                style={{ tintColor: color, width: size, height: size }}
+                source={require("./src/assets/bank.png")}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 }
